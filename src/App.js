b826474ec79637780f5b00cf6bb11a1df22510bc
ikhsan setiawan <ikhsan.setiawan.tif18@polban.ac.id>
2021-02-14 23:29:11 +0700

    fix layout

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import CardTwo from './CardTwo';
 import CardTri from './CardTri';
 import Self from './Self';
 import mars from './image/mars.png';
+import uranus from './image/uranus.png';
+import roket from './image/roket.png';
+import bc2 from './image/bc2.png';
+import bc22 from './image/bc22.png';
+import bc4 from './image/bc4.png';
+
+
 // Little helpers ...
 const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 const NavBarInstance = ({ onSelect, activeKey ,onClik, ...props }) => {
@@ -23,7 +30,6 @@ const NavBarInstance = ({ onSelect, activeKey ,onClik, ...props }) => {
           <Nav.Item eventKey="3"  onClick={() => onClik(2)}>Project</Nav.Item>
           <Nav.Item eventKey="4"  onClick={() => onClik(3)}>Appreciation</Nav.Item>
         </Nav>
-      
       </Navbar.Body>
     </Navbar>
   );
@@ -59,38 +65,38 @@ const Prestasi=() =>{
                   style={{ background: '#eb5056', color: '#fff' }}
                   />}>
         <p class="textPrestasi">09 Desember 2020</p>
-        <p>Lolos Aplikasi Buah hatiku - Kemenparekraf nyatakan.id</p>
+        <p class="textPrestasiDes">Lolos Aplikasi Buah hatiku - Kemenparekraf nyatakan.id</p>
       </Timeline.Item>
      <Timeline.Item dot={<Icon icon="book" size="2x" 
                  style={{ background: '#06c0d7', color: '#fff' }}
                  />}>
         <p  class="textPrestasi">03 Desember 2020</p>
-        <p>Certificate of Completion Course CI/CD for React Native</p>
+        <p class="textPrestasiDes">Certificate of Completion Course CI/CD for React Native</p>
       </Timeline.Item>
       <Timeline.Item dot={<Icon icon="book" size="2x"
                   style={{ background: '#06c0d7', color: '#fff' }}
                   />}>
       <p  class="textPrestasi">23 Agustus 2020</p>
-        <p>Certificate of Completion Course React Native</p>
+        <p class="textPrestasiDes">Certificate of Completion Course React Native</p>
         </Timeline.Item>
       <Timeline.Item dot={<Icon icon="user" size="2x" 
                   style={{ background: '#06c0d7', color: '#fff' }}
                   />}>
         <p  class="textPrestasi">20 Oktober 2019</p>
-        <p>Wakil Ketua Pelaksana LKMM-TD Polban</p>
+        <p class="textPrestasiDes">Wakil Ketua Pelaksana LKMM-TD Polban</p>
       </Timeline.Item>
       <Timeline.Item dot={<Icon icon="camera" size="2x" 
                   style={{ background: '#06c0d7', color: '#fff' }}
                   />}>
         <p  class="textPrestasi">27 Mei 2019</p>
-        <p>Ketua Koordinator Lomba Fotografi Polban</p>
+        <p class="textPrestasiDes">Ketua Koordinator Lomba Fotografi Polban</p>
       </Timeline.Item>
       <Timeline.Item 
       dot={<Icon icon="gamepad" size="2x"  
                  style={{ background: '#06c0d7', color: '#fff' }}
 />}>
         <p class="textPrestasi">27 Mei 2019</p>
-        <p>Ketua Koordinator Turnamen E-Sport PES</p>
+        <p class="textPrestasiDes">Ketua Koordinator Turnamen E-Sport PES</p>
       </Timeline.Item>
     </Timeline>
     </div>
@@ -98,41 +104,43 @@ const Prestasi=() =>{
 } 
 const BlackHole = ()=>{
   return(
-<div class="box"></div>
+<div class="box">
+  
+</div>
   )
 }
 
 const NavHover = () =>{
   return(
 
-    <ButtonToolbar style={{marginLeft:'20%', marginTop:'30%'}}>
+    <ButtonToolbar>
     
    
   <div class ="viewNavCov">
     <div class = "icon">
       <svg height = "80" width="80">
-        <circle cx="40" cy="40" r="35" stroke="white" stroke-width="4" fill="none"></circle>
+        <circle cx="40" cy="40" r="30" stroke="white" stroke-width="4" fill="none"></circle>
       </svg>
       <i class="fab fa-instagram fa-2x">  <a href="https://instagram.com/ihksans/">     <Icon icon="instagram" style={{size:'20%'}} /> 
-</a></i>
+      </a></i>
     </div>
     <div class = "icon">
       <svg height = "80" width="80">
-        <circle cx="40" cy="40" r="35" stroke="white" stroke-width="4" fill="none"></circle>
+        <circle cx="40" cy="40" r="30" stroke="white" stroke-width="4" fill="none"></circle>
       </svg>
       <i class="fab fa-facebook fa-2x">    <a href="https://web.facebook.com/ikhsan.setiawan.3975012">    <Icon icon="facebook-official"  style={{size:'20%'}}/> 
-</a></i>
+    </a></i>
     </div>
     <div class = "icon">
       <svg height = "80" width="80">
-        <circle cx="40" cy="40" r="35" stroke="white" stroke-width="4" fill="none"></circle>
+        <circle cx="40" cy="40" r="30" stroke="white" stroke-width="4" fill="none"></circle>
       </svg>
       <i class="fab fa-whatsapp fa-2x" > <a href="https://api.whatsapp.com/send?phone=628886289639">     <Icon icon="whatsapp" style={{size:'20%'}} /> 
       </a></i>
     </div>
     <div class = "icon">
       <svg height = "80" width="80">
-        <circle cx="40" cy="40" r="35" stroke="white" stroke-width="4" fill="none"></circle>
+        <circle cx="40" cy="40" r="30" stroke="white" stroke-width="4" fill="none"></circle>
       </svg>
       <a href="https://www.linkedin.com/in/ikhsan-setiawan-904a731a4/"><i class="fab fa-twitter fa-2x">      <Icon icon="linkedin" style={{size:'20%'} } /> </i></a>
     </div>
@@ -157,7 +165,7 @@ class App extends React.Component {
       //       <div className="nav-wrapper">
        
 //       </div>
-      <Parallax ref={ref => (this.parallax = ref)} pages={4}>
+      <Parallax ref={ref => (this.parallax = ref)} pages={5}>
          <NavBarInstance appearance="subtle" activeKey={activeKey} onSelect={this.handleSelect} onClik={(n)=>this.parallax.scrollTo(n)}/>
         {/* background color */}
           <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />  {/*//layer1 */}
@@ -169,13 +177,19 @@ class App extends React.Component {
         
         {/* image content layer1 */}
         <ParallaxLayer offset={0} speed={-0.3} style={{ pointerEvents: 'none' }}> 
-          <img src={mars} style={{ width: '20%', marginLeft: '5%', marginTop:'7%' }} />
+        <div class="mars">
+        <img src={mars} style={{ width: '20%', marginLeft: '2%', marginTop:'7%' }} />
+        </div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={-0.3} style={{ pointerEvents: 'none' }}>
-        <img src='image/uranus.png' style={{ width: '40%', marginLeft: '40%', marginTop:'25%' }} />
+       <div class="uranus">
+       <img src={uranus} style={{ width: '40%', marginLeft: '40%', marginTop:'25%' }} />
+       </div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={-0.3} style={{ pointerEvents: 'none' }}>
-        <img src='image/roket.png' style={{ width: '30%', marginLeft: '5%', marginTop:'30%' }} />
+        <div class="roket">
+        <img src={roket} style={{ width: '30%', marginLeft: '5%', marginTop:'30%' }} />
+        </div>
         <BlackHole/>
         </ParallaxLayer>
         {/* image content layer1 */}
@@ -231,24 +245,24 @@ class App extends React.Component {
         
        
          <ParallaxLayer offset={3} speed={-0.3} style={{ pointerEvents: 'none' }}>
-        <img src='image/bc2.png' style={{ width: '100%',height:'100%'}} />
+        <img src={bc2} style={{ width: '100%',height:'100%'}} />
         </ParallaxLayer>
         <ParallaxLayer offset={3.3} speed={0.4} >
-          <img src="image/bc22.png" style={{ display: 'block', width: '100%' ,height:'40%' }} />
+          <img src={bc22} style={{ display: 'block', width: '100%' ,height:'40%' }} />
         </ParallaxLayer>
         <ParallaxLayer offset={4} speed={0.4} >
-          <img src="image/bc4.png" style={{ display: 'block', width: '100%',height:'90%' }} />
+          <img src={bc4} style={{ display: 'block', width: '100%',height:'90%' }} />
         </ParallaxLayer>
 {/* //layer 1// */}
         <ParallaxLayer
           offset={0}
           speed={0.1}
-          style={{ display: 'flex', marginTop:'5%', justifyContent: 'center',marginLeft:'5%' }}>
+          style={{ display: 'flex', marginTop:'5%',marginLeft:'5%',justifyContent: 'center' }}>
           <div class="viewWelcome">
-              <h1 class="textHeader1">Hallo Semua</h1>
-              <h1 class="textHeader1">Aku Ihksan dan ini adalah Blog ku</h1>
+              <p class="textHeader1">Hallo Semua</p>
+              <p class="textHeader1">Aku Ihksan dan ini adalah Blog ku</p>
               <h4 class="textHeader3">Junior Front End Developer</h4>
-              <Button  onClick={() => this.parallax.scrollTo(1)} color="blue" appearance="ghost" style={{marginTop:'3%',alignSelf:'center'}}>
+              <Button  onClick={() => this.parallax.scrollTo(1)} color="blue" appearance="ghost" style={{marginTop:'5%',alignSelf:'center'}}>
                 Let me intoduce my self
               </Button>
               <NavHover/>
@@ -257,15 +271,20 @@ class App extends React.Component {
                   
 
        {/* //layer 2// */}
-
+       <ParallaxLayer
+          offset={0.98}
+          speed={0.1}
+          style={{ display: 'flex', marginTop:'5%', justifyContent: 'center', }}>
+         
+        <p class="textHeaderProject">About Me</p>
+        
+        </ParallaxLayer>
         <ParallaxLayer
-          offset={1}
+          offset={1.1}
           speed={0.1}
 >            
           <div>
-              <div class="header">
-                <h1 class="textHeader1">About Me</h1>
-              </div>
+             
               <div class='flex-container'>
                 <div class="menu">       
                     <Biodata/>
@@ -273,7 +292,7 @@ class App extends React.Component {
                   <div class="textBio2">
                     <Self/>
                     <div class="viewTextBio">
-                      <h7>Sebelumnya saya ucapkan terimakasih telah berkunjung di blog saya. Izinkan saya memperkenalkan diri. Yooo!! perkenalkan nama saya Ikhsan Setiawan. Nama panggilan ihksan. Terkadang orang orang salah menuliskan nama saya, karena selalu dibalik huruf "h" dan "k", hehehe maap ya gais. Saya lahir di Bandung, namun rumah asli saya di Tegal Jawa Tengah. Saya sangat suka belajar hal baru terutama yang berkaitan dengan coding, walau tidak jago jago amat hehehe</h7>
+                      <p>Sebelumnya saya ucapkan terimakasih telah berkunjung di blog saya. Izinkan saya memperkenalkan diri. Yooo!! perkenalkan nama saya Ikhsan Setiawan. Nama panggilan ihksan. Terkadang orang orang salah menuliskan nama saya, karena selalu dibalik huruf "h" dan "k", hehehe maap ya gais. Saya lahir di Bandung, namun rumah asli saya di Tegal Jawa Tengah. Saya sangat suka belajar hal baru terutama yang berkaitan dengan coding, walau tidak jago jago amat hehehe</p>
                     </div>
                     <div class="viewFooterBio"  >    
                     <ButtonToolbar >
@@ -297,7 +316,7 @@ class App extends React.Component {
           speed={-0}
           style={{ display: 'flex', marginTop:'5%', justifyContent: 'center', }}>
          
-        <h1 class="textHeaderProject">Project Experiance</h1>
+        <p class="textHeaderProject">Project Experiance</p>
 
         </ParallaxLayer>
         <ParallaxLayer
@@ -321,8 +340,8 @@ class App extends React.Component {
         <ParallaxLayer
           offset={3}
           speed={-0}
-          style={{ display: 'flex', marginTop:'5%', justifyContent: 'center', }}>
-        <h1 class="textHeaderProject">APPRECIATION</h1>
+          style={{ display: 'flex', justifyContent: 'center', }}>
+        <p class="textHeaderProject">APPRECIATION</p>
         </ParallaxLayer>
         <ParallaxLayer
           offset={3}
@@ -332,14 +351,14 @@ class App extends React.Component {
           <Prestasi/>    
         </ParallaxLayer>
         <ParallaxLayer
-          offset={3.3}
+          offset={3.35}
           speed={-0}
           style={{ display: 'flex', alignItems: 'center',  justifyContent: 'center', }}
          >   
          {/* <div class="viewFooterPres">  */}
           <ButtonToolbar >
               <IconButton  onClick={() => this.parallax.scrollTo(0)} color="red" style={{  }}icon={<Icon icon="arrow-left" />} placement="left" >
-                Back
+                Up we go
               </IconButton>
           </ButtonToolbar>
           {/* </div>    */}
